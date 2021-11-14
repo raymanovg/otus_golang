104b59bf1be11f9f4c5d4e159edba5f3cee30125
Gadel Raymanov <gadel.raymanov@lamoda.ru>
2021-11-14 21:28:15 +0300

    HW7 test for copy and refactoring

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -8,73 +8,77 @@ import (
 )
 
 var (
+	WithProgressBar = true
+
+	ErrSrcFileIsNotExist     = errors.New("src file is not exist")
+	ErrSrcFileIsNotPermitted = errors.New("src file is not permitted")
+	ErrDstFileAlreadyExists  = errors.New("dst file already exists")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(from, to string, offset, limit int64) error {
-	src, err := os.OpenFile(from, os.O_RDONLY, 0)
+func Copy(srcFilePath, dstFilePath string, offset, limit int64) error {
+	src, err := os.OpenFile(srcFilePath, os.O_RDONLY, 0)
 	if err != nil {
-		if os.IsExist(err) {
-			panic("src file is not exist")
+		if !os.IsExist(err) {
+			return ErrSrcFileIsNotExist
 		}
-		if os.IsPermission(err) {
-			panic("src file not permitted")
+		if !os.IsPermission(err) {
+			return ErrSrcFileIsNotPermitted
 		}
-		panic(err)
+		return fmt.Errorf("unknown error: %v", err)
 	}
-
 	defer src.Close()
-
-	dst, err := os.Create(to)
-	if err != nil {
-		if os.IsExist(err) {
-			panic("file already exist")
-		}
-		panic(err)
-	}
-
-	defer dst.Close()
 
 	info, err := src.Stat()
 	if err != nil {
+		return fmt.Errorf("unable to get file info: %v", err)
+	}
+	size := info.Size()
+	if size == 0 {
 		return ErrUnsupportedFile
 	}
-	if info.Size() < offset {
+	if size <= offset {
 		return ErrOffsetExceedsFileSize
 	}
-	_, err = src.Seek(offset, io.SeekStart)
+
+	if _, err := os.Stat(dstFilePath); !errors.Is(err, os.ErrNotExist) {
+		return ErrDstFileAlreadyExists
+	}
+	dst, err := os.Create(dstFilePath)
 	if err != nil {
+		return fmt.Errorf("unable to create dst file: %v", err)
+	}
+	defer dst.Close()
+
+	if _, err = src.Seek(offset, io.SeekStart); err != nil {
 		return fmt.Errorf("unable to set file offset: %v", err)
 	}
-	if limit == 0 {
-		limit = info.Size() - offset
+
+	if limit == 0 || size < limit {
+		limit = size - offset
 	}
 
-	lReader := io.LimitReader(src, limit)
-	pb := NewProgressBar(limit)
-	pbWriter := pb.NewProgressBarWriter(dst)
+	if WithProgressBar {
+		// TODO pb works incorrect if limit great than limit
+		return pbCopyN(dst, io.LimitReader(src, limit), limit)
+	} else {
+		return copyN(dst, io.LimitReader(src, limit), limit)
+	}
+}
 
+func pbCopyN(dst io.Writer, src io.Reader, n int64) error {
+	pb := NewProgressBar(n)
 	pb.Start()
-
-	var written int64
-	buf := make([]byte, 1)
-
-	for {
-		n, err := lReader.Read(buf)
-		if err != nil && err != io.EOF {
-			return fmt.Errorf("unable to read: %v", err)
-		}
-		if n == 0 {
-			break
-		}
-		if _, err = pbWriter.Write(buf[:n]); err != nil {
-			return fmt.Errorf("unable to write: %v", err)
-		}
-		written = written + int64(n)
-	}
-
+	err := copyN(pb.NewProgressBarWriter(dst), src, n)
 	pb.Finish()
 
+	return err
+}
+
+func copyN(dst io.Writer, src io.Reader, n int64) error {
+	if _, err := io.CopyN(dst, src, n); err != nil && err != io.EOF {
+		return fmt.Errorf("not copied: %v", err)
+	}
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -2,4 +2,4 @@ module github.com/raymanovg/otus_golang/hw07_file_copying
 
 go 1.16
 
-require github.com/cheggaaa/pb/v3 v3.0.8 // indirect
+require github.com/stretchr/testify v1.7.0
