c9ed00f1b6692119de247be7c61f4f15cf284951
Gadel Raymanov <gadel.raymanov@lamoda.ru>
2021-12-12 21:09:35 +0300

    HW11 telnet client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,93 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout string
+
+func init() {
+	flag.StringVar(&timeout, "timeout", "10s", "timeout for connection")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "undefined host and port")
+		return
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGKILL)
+
+	timeoutDur, err := time.ParseDuration(timeout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to parse duration")
+		return
+	}
+
+	address := net.JoinHostPort(args[0], args[1])
+	client := NewTelnetClient(address, timeoutDur, os.Stdin, os.Stdout)
+	connErr := client.Connect()
+	if connErr != nil {
+		fmt.Fprintf(os.Stderr, "unable to connect to server %s\n", address)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "...connected to %s\n", address)
+
+	defer client.Close()
+
+	go send(ctx, cancel, client)
+	go receive(ctx, cancel, client)
+
+	<-ctx.Done()
+}
+
+func send(ctx context.Context, cancel context.CancelFunc, client TelnetClient) {
+	defer cancel()
+	for {
+		err := client.Send()
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				fmt.Fprintln(os.Stderr, "...EOF")
+			} else {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			return
+		}
+	}
+}
+
+func receive(ctx context.Context, cancel context.CancelFunc, client TelnetClient) {
+	defer cancel()
+	for {
+		err := client.Receive()
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err == nil {
+				continue
+			}
+			if err == ErrConnectionClosedByPeer {
+				fmt.Fprintln(os.Stderr, "...connection was closed by peer")
+			}
+			return
+		}
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
+)
+
+var (
+	ErrConnectionClosedByPeer = errors.New("connection was closed by peer")
 )
 
 type TelnetClient interface {
@@ -12,10 +21,66 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Telnet struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (t *Telnet) Close() error {
+	t.in.Close()
+	return t.conn.Close()
+}
+
+func (t *Telnet) Connect() error {
+	dialer := net.Dialer{
+		Timeout: t.timeout,
+	}
+	ctx := context.Background()
+	conn, err := dialer.DialContext(ctx, "tcp", t.address)
+	if err != nil {
+		return fmt.Errorf("unble to connect: %w", err)
+	}
+	t.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telnet) Send() error {
+	scanner := bufio.NewScanner(t.in)
+	if !scanner.Scan() {
+		return io.EOF
+	}
+	in := append(scanner.Bytes(), []byte("\n")...)
+	_, err := t.conn.Write(in)
+	if err != nil {
+		return fmt.Errorf("unable to send: %w", err)
+	}
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("unable to read: %w", err)
+	}
+	return nil
+}
+
+func (t *Telnet) Receive() error {
+	scanner := bufio.NewScanner(t.conn)
+	if !scanner.Scan() {
+		return ErrConnectionClosedByPeer
+	}
+	out := append(scanner.Bytes(), []byte("\n")...)
+	_, err := t.out.Write(out)
+	if err != nil {
+		return fmt.Errorf("unable to write: %w", err)
+	}
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Telnet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
